e974598fccfa2bdae20a56d0558b2cc0e5c2497b
Gabriel Ion <150189552+igaby27@users.noreply.github.com>
2025-05-17 19:45:41 +0300

    Final version before deploy

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,31 +1,22 @@
 const API = process.env.REACT_APP_API_URL;
 
-export const get = async (path) => {
-  const res = await fetch(`${API}${path}`);
-  return res.json();
+export const get = async (endpoint) => {
+  const res = await fetch(`${API}${endpoint}`);
+  return await res.json();
 };
 
-export const post = async (path, data) => {
-  const res = await fetch(`${API}${path}`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data),
+export const post = async (endpoint, body) => {
+  const res = await fetch(`${API}${endpoint}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
   });
-  return res.json();
+  return await res.json();
 };
 
-export const del = async (path) => {
-  const res = await fetch(`${API}${path}`, {
-    method: 'DELETE',
+export const del = async (endpoint) => {
+  const res = await fetch(`${API}${endpoint}`, {
+    method: "DELETE",
   });
-  return res.json();
-};
-
-export const put = async (path, data) => {
-  const res = await fetch(`${API}${path}`, {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data),
-  });
-  return res.json();
+  return await res.json();
 };
