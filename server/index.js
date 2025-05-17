0af902374e4b8f57431813e2811f97897941ccca
Gabriel Ion <150189552+igaby27@users.noreply.github.com>
2025-05-17 18:34:56 +0300

    Use dynamic port for Railway

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 //const fetch = require('node-fetch');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
